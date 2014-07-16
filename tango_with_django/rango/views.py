64d203dc473c73b16951b557354edf53bb2ee484
saaguero <aguerosantiale@gmail.com>
2014-07-16 00:20:02 -0300

    Templates and Static Media

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    context = RequestContext(request)
+    context_dict = {'boldmessage': 'I am bold font from the context'}
+    return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-    response = "Rango says Here is the about page <a href='/rango'>Index</a>"
-    return HttpResponse(response)
+    context = RequestContext(request)
+    context_dict = {'boldmessage': 'I am bold font from the context'}
+    return render_to_response('rango/about.html', context_dict, context)
