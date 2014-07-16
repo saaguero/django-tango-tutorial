f9cbec9014d325fd0151f49c92d538ba2f1056d5
saaguero <aguerosantiale@gmail.com>
2014-07-15 21:38:59 -0300

    Finish Django Basics

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -2,3 +2,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hello world!")
+
+def about(request):
+    response = "Rango says Here is the about page <a href='/rango'>Index</a>"
+    return HttpResponse(response)
