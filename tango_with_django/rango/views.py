7784e1a0df125334a053d2a50aa6f340d37d2032
saaguero <aguerosantiale@gmail.com>
2014-07-21 16:35:31 -0300

    Fun with Forms

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -1,6 +1,7 @@
 from django.template import RequestContext
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -26,4 +27,17 @@ def category(request, category_name_url):
     except Category.DoesNotExist:
         pass
 
-    return render(request, 'rango/category.html', context_dict)
+    return render_to_response('rango/category.html', context_dict, context)
+
+def add_category(request):
+    context = RequestContext(request)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = CategoryForm()
+    return render_to_response('rango/add_category.html', {'form':form}, context)
