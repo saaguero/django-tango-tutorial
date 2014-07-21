9743633c9b1377ef203a56203a76cce6e41f6881
saaguero <aguerosantiale@gmail.com>
2014-07-21 12:58:45 -0300

    Use shortcut render instead of render_to_response as it's more compact

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -1,24 +1,20 @@
-from django.http import HttpResponse
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from rango.models import Category, Page
 
 def index(request):
-    context = RequestContext(request)
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
     for category in category_list:
         category.url = category.name.replace(' ', '_')
-    return render_to_response('rango/index.html', context_dict, context)
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    context = RequestContext(request)
     context_dict = {'boldmessage': 'I am bold font from the context'}
-    return render_to_response('rango/about.html', context_dict, context)
+    return render(request, 'rango/about.html', context_dict)
 
 def category(request, category_name_url):
-    context = RequestContext(request)
     category_name = category_name_url.replace('_', ' ')
     context_dict = {'category_name': category_name}
 
@@ -30,4 +26,4 @@ def category(request, category_name_url):
     except Category.DoesNotExist:
         pass
 
-    return render_to_response('rango/category.html', context_dict, context)
+    return render(request, 'rango/category.html', context_dict)
